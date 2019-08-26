9ed693bee53bda88e39e039ea6298fb9e30e9cb5
Alex Felipe <alexfelipevieira@gmail.com>
2019-08-26 10:38:07 -0300

    1.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text('Bem vindo ao Bytebank', textDirection: TextDirection.ltr,));
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                'Craft beautiful UIs',
+                textDirection: TextDirection.ltr,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
