e2cafe51e48497d94292e666047bb0ef9df47294
Alex Felipe <alexfelipevieira@gmail.com>
2019-08-27 09:37:05 -0300

    2.2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
+    body: Column(
+      children: <Widget>[
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100.0'),
+            subtitle: Text('1000'),
+          )
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('200.0'),
+            subtitle: Text('1000'),
+          )
+        ),
+      ],
+    ),
     appBar: AppBar(title: Text('Transferências'),),
     floatingActionButton: FloatingActionButton(
       child: Icon(Icons.add),
