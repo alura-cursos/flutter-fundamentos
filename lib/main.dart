1012d022f3bd2861f2b5d233a64c9c07b9f55c7c
Alex Felipe <alexfelipevieira@gmail.com>
2019-08-28 09:31:04 -0300

    3.2

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,40 @@ class FormularioTransferencia extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Criando Transferência'),),
-        body: Text('teste'));
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 24.0
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                  hintText: '0000'
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 24.0
+                ),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Valor',
+                  hintText: '0.00'
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            RaisedButton(
+              child: Text('Confirmar'),
+            )
+          ],
+        ));
   }
 }
 
